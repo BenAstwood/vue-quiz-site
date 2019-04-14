fe85faa62e75cb286fdb5678c93ea2e386b5834d
ben <Astwood>
2019-04-14 12:45:46 +0100

    Created component structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Header</h1>
+  <div>
+    <b-nav>
+      <b-nav-item disabled>Quiz App</b-nav-item>
+      <b-nav-item disabled>Countter 4/10</b-nav-item>
+    </b-nav>
+  </div>
 </template>
